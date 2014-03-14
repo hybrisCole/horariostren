645e3dfee055180c1d723fe9aed1451352d9b5fd
Alberto Cole <acpii2005@gmail.com>
2014-03-14 01:07:27 -0600

    Seteando adaptadores para Nodejitsu y el Adaptador para Mongo

--- backEnd/config/adapters.js
+++ backEnd/config/adapters.js
@@ -29,11 +29,11 @@ module.exports.adapters = {
   myLocalMySQLDatabase: {
 
     module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
+    host: 'troup.mongohq.com',
+    user: 'nodejitsu',
     // Psst.. You can put your password in config/local.js instead
     // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
-    database: 'YOUR_MYSQL_DB'
+    password: sails.config.dbPassword,
+    database: sails.config.dbName
   }
 };
