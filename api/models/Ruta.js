30ddbdc488c1cfed661218ed7e8586d12e3bad72
Alberto Cole <acpii2005@gmail.com>
2014-03-14 21:02:16 -0600

    Agregando modelos como ejemplo y un controlador de Ruta

---
+++ api/models/Ruta.js
@@ -0,0 +1,15 @@
+/**
+ * Ruta
+ *
+ * @module      :: Model
+ * @description :: Este modelo representa cada una de las rutas que hace el servicio de trenes
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre:'string'
+  }
+
+};
