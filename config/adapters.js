b6ccf40a0e4e352795a07a3d1703aed9b51ae978
Alberto Cole <acpii2005@gmail.com>
2014-03-16 21:48:09 -0600

    cambiando credenciale

--- config/adapters.js
+++ config/adapters.js
@@ -19,11 +19,11 @@ module.exports.adapters = {
   'default': 'mongo',
    mongo:{
      module: 'sails-mongo',
-     host:'troup.mongohq.com',
-     user:'nodejitsu',
-     password:'946c0d7f0d656b2c2a5f35aeffca4112',
+     host:'oceanic.mongohq.com',
+     user:'kei_kurono',
+     password:'123Queso',
      database:'gantz',
-     port:27021,
+     port:10089,
      schema:true
    }
 //
