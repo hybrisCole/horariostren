fc372e8655dc0802833d6e7a6dc0ceeef8f9973b
Alberto Cole <acpii2005@gmail.com>
2014-03-18 01:42:57 -0600

    Agregando Modelo de Precio

---
+++ api/models/Precio.js
@@ -0,0 +1,29 @@
+/**
+ * Precio
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    paradaDesde:{
+      type:'STRING',
+      required:true
+    },
+    paradaHacia:{
+      type:'STRING',
+      required:true
+    },
+    valor:{
+      type:'FLOAT',
+      required:true,
+      min:100,
+      max: 10000
+    }
+  }
+
+};
