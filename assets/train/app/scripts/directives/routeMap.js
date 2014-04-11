c3783a2202f3b6ce6833015871dad09cd2c2b626
Alberto Cole <acpii2005@gmail.com>
2014-04-11 16:39:53 -0600

    Agregando estilos al mapa

--- assets/train/app/scripts/directives/routeMap.js
+++ assets/train/app/scripts/directives/routeMap.js
@@ -4,22 +4,54 @@ angular.module('trenesMobile.directives')
   .directive('routeMap',['$interval', function ($interval) {
     return {
       scope: {
-        objmap : '=',
+        objmap : '='
       },
       restrict: 'A',
       link: function (scope, element, attrs) {
-
-      	console.log(scope.objmap);
-
         var map = new GMaps({
     			div     : '#'+attrs.id,
     			lat     : scope.objmap.markers[1].latitude,
     			lng     : scope.objmap.markers[1].longitude
 				});
 
-        map.setZoom(16);
+        var MY_MAPTYPE_ID = 'custom_style';
 
-        console.log(map.controls);
+        var featureOpts = [
+          {
+            stylers: [
+              { hue: '#007993' },
+              { visibility: 'on' },
+              { gamma: 1 },
+              { weight: 2 },
+              {invert_lightness:true}
+            ]
+          },
+
+          {
+            featureType: 'water',
+            stylers: [
+              { color: '#444344' }
+            ]
+          }
+        ];
+
+        var mapOptions = {
+          mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+          },
+          mapTypeId: MY_MAPTYPE_ID
+        };
+
+        var styledMapOptions = {
+          name: 'Estilo Tren'
+        };
+
+        var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+
+        map.map.setOptions(mapOptions);
+        map.map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+
+        map.setZoom(16);
 
 				runMarkers(scope.objmap.markers);
 
