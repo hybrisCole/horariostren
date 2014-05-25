7b643860974fcc6df5923a65cee9c6734482ebfc
Alberto Cole <acpii2005@gmail.com>
2014-05-25 14:48:13 -0600

    urls de facebook y gmaps

--- assets/train/Gruntfile.js
+++ assets/train/Gruntfile.js
@@ -277,7 +277,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= yeoman.app %>',
-                    src: ['*.html','partials/*.html','views/*.html'],
+                    src: ['*.html','views/*.html'],
                     dest: '<%= yeoman.dist %>'
                 }]
             }
