cb68afb4e3857d7ceb0b66d739b25e18de82e11b
Alberto Cole <acpii2005@gmail.com>
2014-05-26 00:10:33 -0600

    cambiando ng-fx a fade-normal

--- assets/train/app/scripts/controllers/MainController.js
+++ assets/train/app/scripts/controllers/MainController.js
@@ -3,18 +3,18 @@
 angular.module('trenesMobile.controllers', []).controller('MainCtrl', ['$scope', '$rootScope', '$window', '$location', function ($scope, $rootScope, $window, $location) {
   $scope.slideClass = 'gr';
   $rootScope.back = function() {
-    $scope.slideClass = 'fx-fade-left';
+    $scope.slideClass = 'fx-fade-normal';
     $window.history.back();
   };
 
   $scope.go = function(path){
-    $scope.slideClass = 'fx-fade-right';
+    $scope.slideClass = 'fx-fade-normal';
     $location.url(path);
     $rootScope.$broadcast('go');
   };
 
   $scope.goBack = function(path){
-    $scope.slideClass = 'fx-fade-left';
+    $scope.slideClass = 'fx-fade-normal';
     $location.url(path);
     $rootScope.$broadcast('goBack');
   };
